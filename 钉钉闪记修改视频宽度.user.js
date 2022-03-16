7d29dda12af276f2bcdc2c79a34f712cec4ab601
JamesCurtis <49338067+james-curtis@users.noreply.github.com>
2022-03-16 22:39:09 +0800

    1

---
+++ 钉钉闪记修改视频宽度.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         钉钉闪记修改视频宽度
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  钉钉闪记修改视频宽度
+// @author       Curtis
+// @match        https://shanji.dingtalk.com/app/transcribes/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=dingtalk.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    document.getElementsByClassName("H\+NzRG97")[0].style.width='60vw'
+    document.getElementsByTagName('video')[0].style.height='630px'
+    // Your code here...
+})();
